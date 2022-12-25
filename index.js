8706525488205b7c97cccc86300afc94105ec36e
Saurabh Kumar <akgec.saurabh@gmail.com>
2022-12-25 19:47:31 +0530

    secondd

--- index.js
+++ index.js
@@ -2,23 +2,33 @@ const express = require("express");
 const fs = require("fs");
 const cors = require("cors");
 const { dirname } = require("path");
+const https = require("https");
 
-const file = fs.readFileSync("./7184CF8DE9EE76FDC3B06D426284F3CA.txt");
+const key = fs.readFileSync("private.key");
+const cert = fs.readFileSync("certificate.crt");
 const app = express();
 
 // app.use(cors);
 
+const cerd = {
+  key,
+  cert,
+};
+
+const httpsServer = https.createServer(cerd, app);
+httpsServer.listen(8443);
+
 app.get("/api", (req, res, next) => {
-  res.json({ messge: "hello world" });
+  res.json({ messge: "hello work" });
 });
 
-app.get(
-  "/home/ec2-user/hello/7184CF8DE9EE76FDC3B06D426284F3CA.txt",
-  (req, res, next) => {
-    res.sendFile(
-      "C:/Users/akgec/Downloads/7184CF8DE9EE76FDC3B06D426284F3CA.txt"
-    );
-  }
-);
+// app.get(
+//   "/home/ec2-user/hello/7184CF8DE9EE76FDC3B06D426284F3CA.txt",
+//   (req, res, next) => {
+//     res.sendFile(
+//       "C:/Users/akgec/Downloads/7184CF8DE9EE76FDC3B06D426284F3CA.txt"
+//     );
+//   }
+// );
 
 app.listen(3002);
